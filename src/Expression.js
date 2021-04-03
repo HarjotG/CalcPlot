c396ef5ef2808627cf01ac4f0337922a4c7d6a8c
Harjot Grewal <harjot123g@gmail.com>
2021-04-02 18:14:46 -0700

    added more features

--- src/Expression.js
+++ src/Expression.js
@@ -15,4 +15,37 @@ export class Expression {
     derivative() {
         return new Expression("", derivative(this.root, "x")); // make new Expression from derivative
     }
+    left_riemann(a, b, count) {
+        let points = [];
+        let dx = (b - a) / count;
+        for (let i = 0; i < count; i++) {
+            points[3 * i] = { x: (i * dx) + a, y: 0 };
+            points[3 * i + 1] = { x: (i * dx) + a, y: this.evaluate((i * dx) + a) };
+            points[3 * i + 2] = { x: (i * dx) + a + dx, y: this.evaluate((i * dx) + a) };
+        }
+        points[points.length] = { x: b, y: 0 };
+        return points;
+    }
+    right_riemann(a, b, count) {
+        let points = [];
+        let dx = (b - a) / count;
+        for (let i = 0; i < count; i++) {
+            points[3 * i] = { x: (i * dx) + a, y: 0 };
+            points[3 * i + 1] = { x: (i * dx) + a, y: this.evaluate((i * dx) + a + dx) };
+            points[3 * i + 2] = { x: (i * dx) + a + dx, y: this.evaluate((i * dx) + a + dx) };
+        }
+        points[points.length] = { x: b, y: 0 };
+        return points;
+    }
+    mid_riemann(a, b, count) {
+        let points = [];
+        let dx = (b - a) / count;
+        for (let i = 0; i < count; i++) {
+            points[3 * i] = { x: (i * dx) + a, y: 0 };
+            points[3 * i + 1] = { x: (i * dx) + a, y: this.evaluate((i * dx) + a + dx / 2) };
+            points[3 * i + 2] = { x: (i * dx) + a + dx, y: this.evaluate((i * dx) + a + dx / 2) };
+        }
+        points[points.length] = { x: b, y: 0 };
+        return points;
+    }
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-var _a, _b, _c;
+var _a, _b, _c, _d;
 import { Graph } from './Graph.js';
 import katex from 'katex';
 import { Expression } from './Expression.js';
@@ -9,25 +9,57 @@ document.getElementById('function').value = "x^2"; // set the default value for 
 let graph = new Graph(canvas, scale, dx);
 let expressions = [];
 expressions.length = 3;
+let points = [];
+let mouse_down_flag = false; // keep track of whether mouse is clicked down
+let lastX = 0; // keep track of where the last mouse move was
+let lastY = 0;
 canvas.addEventListener('wheel', function (event) {
-    graph.rescale(event.deltaY / 50);
+    graph.rescale(event.deltaY / 50, lastX - canvas.getBoundingClientRect().left, lastY - canvas.getBoundingClientRect().top);
     graph.drawExpressions(expressions);
+    graph.draw_points(points);
+});
+canvas.addEventListener('mousemove', function (event) {
+    if (mouse_down_flag) {
+        graph.move_center(event.clientX - lastX, event.clientY - lastY);
+        graph.drawExpressions(expressions);
+        graph.draw_points(points);
+    }
+    lastX = event.clientX;
+    lastY = event.clientY;
+});
+canvas.addEventListener('mousedown', function (event) {
+    mouse_down_flag = true;
+    lastX = event.clientX;
+    lastY = event.clientY;
+});
+canvas.addEventListener('mouseup', function (event) {
+    mouse_down_flag = false;
+    lastX = event.clientX;
+    lastY = event.clientY;
 });
 window.addEventListener('resize', function () {
     graph.resize();
     graph.drawExpressions(expressions);
+    graph.draw_points(points);
 }); // resize the canvas when the window is resized
 (_a = document.getElementById("Plot")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", function () {
     let func = document.getElementById('function').value;
     expressions[0] = new Expression(func);
     graph.drawExpressions(expressions);
+    points = [];
     katex.render(func, document.getElementById('katex'));
 });
 (_b = document.getElementById("Clear")) === null || _b === void 0 ? void 0 : _b.addEventListener("click", function () {
     graph.clearPlot();
     expressions = [];
+    points = [];
 });
 (_c = document.getElementById("Derivative")) === null || _c === void 0 ? void 0 : _c.addEventListener("click", function () {
     expressions[1] = expressions[0].derivative();
     graph.drawExpressions(expressions);
+    graph.draw_points(points);
+});
+(_d = document.getElementById("Riemann")) === null || _d === void 0 ? void 0 : _d.addEventListener("click", function () {
+    points = expressions[0].mid_riemann(-5, 5, 8);
+    graph.draw_points(points);
 });
